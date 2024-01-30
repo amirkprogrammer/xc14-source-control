596f1f441534df011c28e04fae77f326e32ae22b
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-01-30 12:33:50 -0800

    Initial Commit

---
+++ xc14-source-control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xc14-source-control
+//
+//  Created by Amir Kabiri on 1/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xc14-source-control/xc14_source_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  xc14_source_controlApp.swift
+//  xc14-source-control
+//
+//  Created by Amir Kabiri on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct xc14_source_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
